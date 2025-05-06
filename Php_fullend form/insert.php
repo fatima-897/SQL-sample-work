1ec6ce5aa2e207886071fc2135cecd5a6019e892
Fatima Nahid-Dev <fatimanahid341@gmail.com>
2025-05-06 15:39:08 +0500

    Update insert.php

--- Php_fullend form/insert.php
+++ Php_fullend form/insert.php
@@ -1,13 +1,11 @@
-<?php 
-include "db.php";
-echo "connected";
+<?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POSR") {
-    $name = $_POST["name"];
-   $email = $_POST["email"];
-    $conn->query("INSERT INTO users (name, email) VALUES ('$name', '$email')");
+include 'db.php';
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $conn->query("INSERT INTO users (name, email) VALUES ('$name', '$email')");
 }
-header("Location :index.php");
-
+header("Location: index.php");
 ?>
