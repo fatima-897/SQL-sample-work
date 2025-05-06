abfd7ae2bda19a3965c24caa6c2e94af7dac94c1
Fatima Nahid-Dev <fatimanahid341@gmail.com>
2025-05-06 15:38:07 +0500

    Update db.php

--- Php_fullend form/db.php
+++ Php_fullend form/db.php
@@ -1,15 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container mt-4 w-50">
-
 <?php
 $servername = "localhost";
 $username = "root";
@@ -24,7 +12,3 @@ if ($conn->connect_error) {
 }
 
 ?>
-
- </div>
-</body>
-</html>
